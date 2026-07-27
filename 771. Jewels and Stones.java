a0872baa823b180e9de09df8ae310e4bd0e1fa3a
shaikjaza5011-lgtm <shaikjaza5011@gmail.com>
2026-07-27 08:39:06 +0530

    Create 771. Jewels and Stones.java

---
+++ 771. Jewels and Stones.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        HashSet<Character> set = new HashSet<>();
+
+        // Store all jewels in the HashSet
+        for (char ch : jewels.toCharArray()) {
+            set.add(ch);
+        }
+
+        int count = 0;
+
+        // Count matching stones
+        for (char ch : stones.toCharArray()) {
+            if (set.contains(ch)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
